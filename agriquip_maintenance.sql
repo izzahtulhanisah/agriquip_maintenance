f66b951d253346cd75e85d159062570eb1399353
Izzah <izzahtul.hanisah@gmail.com>
2018-10-30 00:25:34 +0800

    database

--- agriquip_maintenance.sql
+++ agriquip_maintenance.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Oct 23, 2018 at 02:12 AM
+-- Generation Time: Oct 29, 2018 at 04:24 PM
 -- Server version: 5.7.23
 -- PHP Version: 7.2.10
 
@@ -35,16 +35,16 @@ CREATE TABLE IF NOT EXISTS `company` (
   `company_code` varchar(200) NOT NULL,
   `description` varchar(200) NOT NULL,
   PRIMARY KEY (`company_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `company`
 --
 
 INSERT INTO `company` (`company_id`, `company_name`, `company_code`, `description`) VALUES
-(1, 'Malaysia Airlines', 'MH', '5 years contract'),
-(2, 'Pos Aviation', 'PA', '5 equipment types involved'),
-(3, 'Malindo Air', 'MA', '3 years contract. PIC: Mr. Tang');
+(1, 'Malaysia Airlines', 'MH', '3 years contract'),
+(2, 'Pos Aviation', 'PA', 'Provides two equipments'),
+(3, 'AirAsia', 'AA', 'PIC: Matt Swan');
 
 -- --------------------------------------------------------
 
@@ -59,36 +59,20 @@ CREATE TABLE IF NOT EXISTS `equipment` (
   `equipment_no` varchar(200) NOT NULL,
   `manufacture_year` varchar(200) NOT NULL,
   `seriel_no` varchar(200) NOT NULL,
-  `owner` varchar(200) NOT NULL,
   `remark` varchar(200) NOT NULL,
+  `type_id` int(20) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=25 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `equipment`
 --
 
-INSERT INTO `equipment` (`id`, `type_name`, `equipment_no`, `manufacture_year`, `seriel_no`, `owner`, `remark`) VALUES
-(3, 'MHBT - Baggage Trolley', 'MHBT003', '2013', '1212', 'AirAsia', ''),
-(5, 'MHBT - Baggage Trolley', 'MHBT005', '', '', '', ''),
-(4, 'MHBT - Baggage Trolley', 'MHBT004', '', '', '', ''),
-(2, 'MHBT - Baggage Trolley', 'MHBT002', '', '', '', ''),
-(1, 'MHBT - Baggage Trolley', 'MHBT001', '', '', '', ''),
-(6, 'MHBT - Baggage Trolley', 'MHBT006', '', '', '', ''),
-(7, 'MHBT - Baggage Trolley', 'MHBT007', '', '', '', ''),
-(8, 'MHBT - Baggage Trolley', 'MHBT008', '', '', '', ''),
-(9, 'MHBT - Baggage Trolley', 'MHBT009', '', '', '', ''),
-(10, 'MHBT - Baggage Trolley', 'MHBT010', '', '', '', ''),
-(11, 'MHBT - Baggage Trolley', 'MHBT011', '', '', '', ''),
-(12, 'MHBT - Baggage Trolley', 'MHBT012', '', '', '', ''),
-(13, 'MHBT - Baggage Trolley', 'MHBT013', '', '', '', ''),
-(14, 'MHBT - Baggage Trolley', 'MHBT014', '', '', '', ''),
-(15, 'MHBT - Baggage Trolley', 'MHBT015', '', '', '', ''),
-(16, 'MHBT - Baggage Trolley', 'MHBT016', '', '', '', ''),
-(17, 'MHBT - Baggage Trolley', 'MHBT017', '', '', '', ''),
-(18, 'MHBT - Baggage Trolley', 'MHBT018', '', '', '', ''),
-(19, 'MHBT - Baggage Trolley', 'MHBT019', '', '', '', ''),
-(20, 'MHBT - Baggage Trolley', 'MHBT020', '', '', '', '');
+INSERT INTO `equipment` (`id`, `type_name`, `equipment_no`, `manufacture_year`, `seriel_no`, `remark`, `type_id`) VALUES
+(1, 'AAPD - Pallet Dolley', 'AAPD001', '2012', '12345', 'Started on 1/10/2017', 3),
+(2, 'MHPD - Pallet Dolley', 'MHPD001', '2012', '1002', 'To be inspected on 12/11', 4),
+(3, 'PACT - Container Trolley', 'PACT003', '2013', '1313', 'Started on 1/10/2017', 4),
+(4, 'MHBT - Baggage Trolley', 'MHBT001', '2012', '1001', 'Started on 1/10/2017', 4);
 
 -- --------------------------------------------------------
 
@@ -104,20 +88,18 @@ CREATE TABLE IF NOT EXISTS `equipment_type` (
   `company_id` int(20) NOT NULL,
   `company_name` varchar(200) NOT NULL,
   PRIMARY KEY (`type_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=34 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `equipment_type`
 --
 
 INSERT INTO `equipment_type` (`type_id`, `type_name`, `description`, `company_id`, `company_name`) VALUES
-(29, 'MHPD - Pallet Dolley', 'Pallettes', 0, '1'),
-(28, 'MHBT - Baggage Trolley', 'A massive trolley', 0, '1'),
-(30, 'MHCT - MHCT', '', 0, '2'),
-(27, 'MHCT - Container Trolley', 'Carry lotso containers', 0, '1'),
-(31, 'PABT - Baggage Trolley', 'One hundred trolleys', 0, ''),
-(32, 'PACT - Container Trolley', 'Two hundreds trolleys', 0, 'Pos Aviation'),
-(33, 'MAPD - Pallet Dolley', '', 3, 'Malindo Air');
+(1, 'MHBT - Baggage Trolley', 'For lotso baggages', 1, 'Malaysia Airlines'),
+(2, 'PACT - Container Trolley', 'Massive trolleys', 2, 'Pos Aviation'),
+(3, 'AAPD - Pallet Dolley', 'Wide and large', 3, 'AirAsia'),
+(4, 'MHPD - Pallet Dolley', 'Blue color', 1, 'Malaysia Airlines'),
+(5, 'MHCT - Container Trolley', 'Bigger trolley', 3, 'Malaysia Airlines');
 
 -- --------------------------------------------------------
 
@@ -132,17 +114,15 @@ CREATE TABLE IF NOT EXISTS `part` (
   `part_no` varchar(200) NOT NULL,
   `part_name` varchar(200) NOT NULL,
   PRIMARY KEY (`part_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `part`
 --
 
 INSERT INTO `part` (`part_id`, `type_name`, `part_no`, `part_name`) VALUES
-(3, 'MHCT - Container Trolley', 'CT-6.0', 'Roller A'),
-(2, 'MHBT - Baggage Trolley', 'BT-3.1', 'A-Frame'),
-(4, 'MHPD - Pallet Dolley', 'PD-3.0', 'Rigid Fork'),
-(5, '', '', '');
+(1, 'MHBT - Baggage Trolley', 'BT-3.1', 'A-Frame'),
+(2, 'PACT - Container Trolley', 'CT-1.0', 'Tow Bar');
 
 -- --------------------------------------------------------
 
@@ -160,16 +140,87 @@ CREATE TABLE IF NOT EXISTS `repair` (
   `date_released` varchar(20) NOT NULL,
   `remark` varchar(200) NOT NULL,
   PRIMARY KEY (`repair_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `repair`
 --
 
 INSERT INTO `repair` (`repair_id`, `type_name`, `equipment_no`, `date_received`, `submitted_by`, `date_released`, `remark`) VALUES
-(11, 'MHBT - Baggage Trolley', 'MHBT013', '2018-10-01', '', '2018-10-05', ''),
-(12, 'MHBT - Baggage Trolley', 'MHBT014', '2018-10-08', '', '2018-10-12', ''),
-(14, 'MHBT - Baggage Trolley', 'MHBT011', '2018-10-23', 'from session', '2018-10-24', 'Inspection');
+(1, 'MHBT - Baggage Trolley', 'MHBT001', '2018-10-01', 'from session', '2018-10-04', 'Changed rollers'),
+(4, 'MHBT - Baggage Trolley', 'MHBT001', '2018-10-22', 'from session', '2018-10-24', 'Change wheels'),
+(5, 'MHBT - Baggage Trolley', 'MHBT001', '', '', '', ''),
+(6, 'MHBT - Baggage Trolley', 'MHBT001', '', '', '', ''),
+(7, 'MHBT - Baggage Trolley', 'MHBT001', '2018-10-30', '', '2018-10-31', ''),
+(8, 'PACT - Container Trolley', 'PACT003', '', '', '', ''),
+(9, 'MHBT - Baggage Trolley', 'MHBT001', '', '', '', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `repair_part`
+--
+
+DROP TABLE IF EXISTS `repair_part`;
+CREATE TABLE IF NOT EXISTS `repair_part` (
+  `repairpart_id` int(20) NOT NULL AUTO_INCREMENT,
+  `part_no` varchar(200) NOT NULL,
+  `quantity` int(20) NOT NULL,
+  `repair_id` int(20) NOT NULL,
+  PRIMARY KEY (`repairpart_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `repair_part`
+--
+
+INSERT INTO `repair_part` (`repairpart_id`, `part_no`, `quantity`, `repair_id`) VALUES
+(1, 'CT-1.0', 1, 4),
+(2, '$part_no', 2, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `staff`
+--
+
+DROP TABLE IF EXISTS `staff`;
+CREATE TABLE IF NOT EXISTS `staff` (
+  `id` int(20) NOT NULL AUTO_INCREMENT,
+  `staff_id` varchar(20) NOT NULL,
+  `staff_password` varchar(20) NOT NULL,
+  `staff_name` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `staff`
+--
+
+INSERT INTO `staff` (`id`, `staff_id`, `staff_password`, `staff_name`) VALUES
+(1, 'staff', 'staff', 'staff ads');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `supervisor`
+--
+
+DROP TABLE IF EXISTS `supervisor`;
+CREATE TABLE IF NOT EXISTS `supervisor` (
+  `id` int(20) NOT NULL AUTO_INCREMENT,
+  `supervisor_id` varchar(20) NOT NULL,
+  `supervisor_password` varchar(20) NOT NULL,
+  `supervisor_name` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `supervisor`
+--
+
+INSERT INTO `supervisor` (`id`, `supervisor_id`, `supervisor_password`, `supervisor_name`) VALUES
+(1, 'supervisor', 'supervisor', 'supervisor ads');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
